e9f22e0bde70343b6e29c1f0830fe99083806c82
정현우\jhw50 <jhw504166@gmail.com>
2023-10-16 13:17:14 +0900

    정현우 : 태그 추가 터렛이 몬스터 태그를 가진 오브젝트를 찾아서 바라보게 구현 / 터렛을 설치하면 FillAmount가 10초에 걸져 줄어들고 10초가 지나면 디스트로이

---
+++ VRTowerDefense/Assets/02. TestSceneFolder/03. JHW/Scripts/LookAtTarget.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour
+{
+    public string targetTag = "Finish";  // 바라볼 대상의 태그
+    public float rotationSpeed = 5.0f;   // 회전 속도
+
+    void Update()
+    {
+        // 주변에 특정 태그를 가진 오브젝트를 찾습니다.
+        GameObject[] targets = GameObject.FindGameObjectsWithTag(targetTag);
+
+        if (targets.Length > 0)
+        {
+            // 가장 가까운 대상을 찾습니다.
+            GameObject closestTarget = FindClosestTarget(targets);
+
+            if (closestTarget != null)
+            {
+                // 대상을 향한 방향을 계산합니다.
+                Vector3 direction = closestTarget.transform.position - transform.position;
+
+                // 대상을 바라보기 위한 회전을 계산합니다.
+                Quaternion lookRotation = Quaternion.LookRotation(direction);
+
+                // 부드러운 회전을 위해 회전을 보간합니다.
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    GameObject FindClosestTarget(GameObject[] targets)
+    {
+        GameObject closestTarget = null;
+        float closestDistance = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+
+        foreach (GameObject target in targets)
+        {
+            float distance = Vector3.Distance(target.transform.position, currentPosition);
+
+            if (distance < closestDistance)
+            {
+                closestTarget = target;
+                closestDistance = distance;
+            }
+        }
+
+        return closestTarget;
+    }
+}
