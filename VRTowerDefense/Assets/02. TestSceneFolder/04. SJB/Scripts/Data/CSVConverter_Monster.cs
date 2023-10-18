19eb7cdc39263c7823c5239f4094738c99c5c009
JB <sjbh3mc@gmail.com>
2023-10-18 13:12:11 +0900

    심준보 : 보스 및 졸개 bullet gethit 메서드 수정, Data_Boss, Data_Minion, Data_Projectile 스크립트 추가, Mod : Boss and Minion Script GetHit() modified, Add : Data_Boss, Data_Minion, Data_Projectile Added

---
+++ VRTowerDefense/Assets/02. TestSceneFolder/04. SJB/Scripts/Data/CSVConverter_Monster.cs
@@ -0,0 +1,123 @@
+using UnityEditor;
+using UnityEngine;
+
+public class CSVConverter_Monster : MonoBehaviour
+{
+    private static CSVConverter_Monster converter_Instance;
+    // 싱글턴으로 만들기
+    public static CSVConverter_Monster Instance
+    {
+        get
+        {
+            if (converter_Instance == null)
+            {
+                converter_Instance = FindObjectOfType<CSVConverter_Monster>();
+            }
+            return converter_Instance;
+        }
+    }
+
+
+    // 읽어온 TextAsset 파일의 정보를 담을 변수
+    private TextAsset itemDataFile = default;
+
+    // 최종적으로 데이터 파일을 쪼개서 담을 배열
+    public string[] itemDataList { get; private set; }
+
+    // 외부에서 접근할 csv 파일 행 크기 (test csv: 17)
+    public int csvRowCount { get; private set; }
+    // 외부에서 접근할 csv 파일 열 크기 (test csv: 19)
+    public int csvColumnCount { get; private set; }
+
+
+
+    // { csv 파일 데이터 변수
+    public int[] CSVID { get; private set; }
+    public string[] Description { get; private set; }
+    public int[] UnitID { get; private set; }
+    public int[] Price { get; private set; }
+    public int[] TotalNum { get; private set; }
+    // } csv 파일 데이터 변수
+
+
+    private void Awake()
+    {
+        ReadCSV();
+        CheckCSVRowColumn();
+
+        itemDataList = new string[csvRowCount * csvColumnCount];
+
+        SplitData();
+        InitArrays();
+
+        SortData();
+        Test();
+    }
+
+
+    private void ReadCSV()
+    {
+        itemDataFile = Resources.Load<TextAsset>("CSVData/test");//여역시
+    }
+
+    private void CheckCSVRowColumn()
+    {
+        string dataTrimNull_ = itemDataFile.text.TrimEnd();
+        string[] tempRow_ = default;
+        string[] tempColumn_ = default;
+
+        tempRow_ = dataTrimNull_.Split("\n");
+        csvRowCount = tempRow_.Length;
+
+        tempColumn_ = tempRow_[0].Split(",");
+        csvColumnCount = tempColumn_.Length;
+    }
+
+    private void SplitData()
+    {
+        string dataTrimNull_ = itemDataFile.text.TrimEnd();
+        string[] splitdata_ = default;
+
+        splitdata_ = dataTrimNull_.Split(new char[] {'\n' , ','});
+
+
+        for (int i = 0; i < csvRowCount * csvColumnCount; i++)
+        {
+            itemDataList[i] = splitdata_[i];
+        }
+    }
+
+    private void InitArrays()
+    {
+        CSVID = new int[csvRowCount];
+        Description = new string[csvRowCount];
+        UnitID = new int[csvRowCount];
+        Price = new int[csvRowCount];
+        TotalNum = new int[csvRowCount];
+    }
+
+    private void SortData() 
+    {
+        // TODO : 매직넘버 없애기
+        for (int i = 0; i < csvRowCount - 1; i++)
+        {
+            CSVID[i] = int.Parse(itemDataList[csvColumnCount * (i + 1)]);
+            Description[i] = itemDataList[(csvColumnCount * (i + 1)) + 1];
+            UnitID[i] = int.Parse(itemDataList[(csvColumnCount * (i + 1)) + 2]);
+            Price[i] = int.Parse(itemDataList[(csvColumnCount * (i + 1)) + 3]);
+            TotalNum[i] = int.Parse(itemDataList[(csvColumnCount * (i + 1)) + 4]);
+        }
+    }
+
+    private void Test() 
+    {
+        for (int i = 0; i < csvRowCount - 1; i++) 
+        {
+            Debug.Log(CSVID[i]);
+            Debug.Log(Description[i]);
+            Debug.Log(UnitID[i]);
+            Debug.Log(Price[i]);
+            Debug.Log(TotalNum[i]);
+        }
+    }
+}
