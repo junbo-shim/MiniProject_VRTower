32aa6fe5e076dc3da6b02439f849138359f152b7
정현우\jhw50 <jhw504166@gmail.com>
2023-10-26 09:57:14 +0900

    정현우 : Add : CSV에서 데이터를 받아온 스크립터블오브젝트 데이터를 기존 데이터에 대입

--- VRTowerDefense/Assets/02. TestSceneFolder/03. JHW/Scripts/BulletData.cs
+++ VRTowerDefense/Assets/02. TestSceneFolder/03. JHW/Scripts/BulletData.cs
@@ -6,16 +6,18 @@ public class BulletData
     public float damage;
     public float speed;
     public float slow;
+
     public string additionalData;
+    public int debuffDuration;
 
 
-    public BulletData(float _damage, float _speed, string _additionalData, float _slow)
+    public BulletData(float damage_, float speed_, string additionalData_, float slow_ , int debuffDuration_)
     {
-        damage = _damage;
-        speed = _speed;
-        slow = _slow;
-
-        additionalData = _additionalData;
+        damage = damage_;
+        speed = speed_;
+        slow = slow_;
+        debuffDuration = debuffDuration_;
+        additionalData = additionalData_;
         this.slow = slow;
     }
 }
